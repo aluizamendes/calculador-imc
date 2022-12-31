dc6026f2799cf7a5af9fdb78103f72e9241aca36
Ana Luiza <analuizamdv@gmail.com>
2022-12-31 14:02:28 -0300

    adding icons for success and error message

--- js/main.js
+++ js/main.js
@@ -18,7 +18,8 @@ btnCalcular.addEventListener("click", () => {
 
   // se os campos estiverem vazios mensagem de erro
   if(altura.value == "" || peso.value == "" || altura.value == null || peso.value == null) {
-    resultadoTitle.innerText = `É necessário informar os dados acima para calcular seu imc.`;
+    resultadoTitle.classList.add("error");
+    resultadoTitle.innerHTML = `<img src="assets/error.svg" alt="imagem de feedback do calculo"> É necessário informar os dados acima para calcular seu imc.`;
 
     resultadoImc.innerText = "";
     classificacao.innerText = "";
@@ -27,54 +28,51 @@ btnCalcular.addEventListener("click", () => {
   }
 
   else {
-  // calcula imc
-  let imc = peso.value / (altura.value * altura.value);
+    // calcula imc
+    let imc = peso.value / (altura.value * altura.value);
+      
+    resultadoTitle.classList.add("success");
+    resultadoTitle.innerHTML = `<img src="assets/success.svg" alt="imagem de feedback do calculo">Seu IMC foi calculado!`;
 
-  resultadoTitle.innerText = `Seu IMC foi calculado!`;
+    // se o valor do nome estiver vazio
+    if(nome.value == "") {
+      // exibe resultado na tela
+      resultadoImc.innerText = `O seu imc é igual a: ${imc.toFixed(2)}.`;
+    } 
+    else {
+      // exibe resultado na tela
+      resultadoImc.innerText = `${nome.value}, o seu IMC é igual a: ${imc.toFixed(2)}.`;
+    } 
 
-  // se o valor do nome estiver vazio
-  if(nome.value == "") {
-    // exibe resultado na tela
-    resultadoImc.innerText = `O seu imc é igual a: ${imc.toFixed(2)}.`;
-  } 
-  else {
-    // exibe resultado na tela
-    resultadoImc.innerText = `${nome.value}, o seu IMC é igual a: ${imc.toFixed(2)}.`;
-  } 
+    // exibir a classificação do imc
+    if(imc <= 18.4 && imc >= 16) {
+      classificacao.innerHTML = "<strong>Classificação:</strong><br> Magreza.";
+    }
+    else if(imc >= 18.5 && imc < 25) {
+      classificacao.innerHTML = "<strong>Classificação:</strong><br> Peso ideal.";
+    }
+    else if(imc > 25 && imc < 30) {
+      classificacao.innerHTML = "<strong>Classificação:</strong><br> Sobrepeso, risco aumentado.";
+    }
+    else if(imc > 30 && imc < 35) {
+      classificacao.innerHTML = "<strong>Classificação:</strong><br> Obesidade Grau I, risco moderado.";
+    }
+    else if(imc > 35 && imc < 40) {
+      classificacao.innerHTML = "<strong>Classificação:</strong><br> Obesidade Grau II, risco grave.";
+    }
+    else if(imc > 40) {
+      classificacao.innerHTML = "<strong>Classificação:</strong><br> Obesidade Grau III, risco muito grave!";
+    }
 
-  // exibir a classificação do imc
-  if(imc <= 18.4 && imc >= 16) {
-    classificacao.innerHTML = "<strong>Classificação:</strong> Magreza.";
-  }
-  else if(imc >= 18.5 && imc < 25) {
-    classificacao.innerHTML = "<strong>Classificação:</strong> Peso ideal.";
-  }
-  else if(imc > 25 && imc < 30) {
-    classificacao.innerHTML = "<strong>Classificação:</strong> Sobrepeso, risco aumentado.";
-  }
-  else if(imc > 30 && imc < 35) {
-    classificacao.innerHTML = "<strong>Classificação:</strong> Obesidade Grau I, risco moderado.";
-  }
-  else if(imc > 35 && imc < 40) {
-    classificacao.innerHTML = "<strong>Classificação:</strong> Obesidade Grau II, risco grave.";
-  }
-  else if(imc > 40) {
-    classificacao.innerHTML = "<strong>Classificação:</strong> Obesidade Grau III, risco muito grave!";
-  }
+    // limpa os valores do input
+    nome.value = "";
+    altura.value = "";
+    peso.value = "";
 
-
-
-  // limpa os valores do input
-  nome.value = "";
-  altura.value = "";
-  peso.value = "";
-
-  nome.disabled = true;
-  altura.disabled = true;
-  peso.disabled = true;
-  btnCalcular.disabled = true;
-  btnRecarregar.style.display = "block";
-
-
+    nome.disabled = true;
+    altura.disabled = true;
+    peso.disabled = true;
+    btnCalcular.disabled = true;
+    btnRecarregar.style.display = "block";
   }  
 });
